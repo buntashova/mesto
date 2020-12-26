068b672b9c9fa5f71885c92a05732604e057a579
Valeria Buntashova <vbuntashova@mail.ru>
2020-12-26 21:34:56 +0700

    fix: correction after review

--- scripts/index.js
+++ scripts/index.js
@@ -2,33 +2,31 @@ const editButton = document.querySelector(".profile__button-edt");
 const popup = document.querySelector(".popup");
 const closeButton = document.querySelector(".popup__close");
 
-const nameInput = document.querySelector(".popup__input-name");
-const bioInput = document.querySelector(".popup__input-bio");
+const nameInput = document.querySelector(".popup__input_type_name");
+const bioInput = document.querySelector(".popup__input_type_bio");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 const popupForm = document.querySelector(".popup__container");
 
-editButton.addEventListener("click", () => {
-  popup.classList.add("popup_opened");
-})
-
-closeButton.addEventListener("click", () => {
-  popup.classList.remove("popup_opened");
-})
-
-nameInput.value = profileName.textContent;
-bioInput.value = profileDescription.textContent;
-
 function handleFormSubmit(evt) {
   evt.preventDefault();
 
-  const nameValue = nameInput.value;
-  const bioValue = bioInput.value;
+  profileName.textContent = nameInput.value;;
+  profileDescription.textContent = bioInput.value;;
 
-  profileName.textContent = nameValue;
-  profileDescription.textContent = bioValue;
+  popupClose();
+}
 
+function popupOpen() {
+  nameInput.value = profileName.textContent;
+  bioInput.value = profileDescription.textContent;
+  popup.classList.add("popup_opened");
+}
+
+function popupClose() {
   popup.classList.remove("popup_opened");
 }
 
+editButton.addEventListener("click", popupOpen);
+closeButton.addEventListener("click", popupClose);
 popupForm.addEventListener("submit", handleFormSubmit);

736aea144ce4403c6d650ce721c677605d2350e3
Valeria Buntashova <vbuntashova@mail.ru>
2021-01-15 15:40:11 +0700

    feat: create open and close new add popup

--- scripts/index.js
+++ scripts/index.js
@@ -25,15 +25,22 @@ const cards = [
   }
 ];
 
+const editPopup = document.querySelector(".popup_edt");
 const editButton = document.querySelector(".profile__button-edt");
-const popup = document.querySelector(".popup");
-const closeButton = document.querySelector(".popup__close");
+const closeEdit = editPopup.querySelector(".popup__close");
+
+const addPopup = document.querySelector(".popup_add");
+const addButton = document.querySelector(".profile__button-add");
+const closeAdd = addPopup.querySelector(".popup__close");
+
+const edtForm = editPopup.querySelector(".popup__container");
+const addForm = addPopup.querySelector(".popup__container");
+
 
 const nameInput = document.querySelector(".popup__input_type_name");
 const bioInput = document.querySelector(".popup__input_type_bio");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
-const popupForm = document.querySelector(".popup__container");
 
 const elementsTemplate = document.querySelector(".elements__template").content;
 const elementsSection = document.querySelector(".elements");
@@ -42,19 +49,44 @@ const elementsSection = document.querySelector(".elements");
 function handleFormSubmit(evt) {
   evt.preventDefault();
 
-  profileName.textContent = nameInput.value;;
-  profileDescription.textContent = bioInput.value;;
+  if (evt.target === edtForm) {
+    profileName.textContent = nameInput.value;;
+    profileDescription.textContent = bioInput.value;
 
-  popupClose();
+    close(editPopup);
+  }
+  if (evt.target === addForm) {
+    close(addPopup);
+  }
+
 }
 
-function popupOpen() {
-  nameInput.value = profileName.textContent;
-  bioInput.value = profileDescription.textContent;
+function popupOpen(evt) {
+  if (evt.target === editButton) {
+    nameInput.value = profileName.textContent;
+    bioInput.value = profileDescription.textContent;
+    open(editPopup);
+  }
+  if (evt.target === addButton) {
+    open(addPopup);
+  }
+
+}
+
+function open(popup) {
   popup.classList.add("popup_opened");
 }
 
-function popupClose() {
+function popupClose(evt) {
+  if (evt.target === closeEdit) {
+    close(editPopup);
+  }
+  if (evt.target === closeAdd) {
+    close(addPopup);
+  }
+}
+
+function close(popup) {
   popup.classList.remove("popup_opened");
 }
 
@@ -74,5 +106,12 @@ function renderCards(card) {
 render();
 
 editButton.addEventListener("click", popupOpen);
-closeButton.addEventListener("click", popupClose);
-popupForm.addEventListener("submit", handleFormSubmit);
+addButton.addEventListener("click", popupOpen);
+
+closeEdit.addEventListener("click", popupClose);
+closeAdd.addEventListener("click", popupClose);
+
+edtForm.addEventListener("submit", handleFormSubmit);
+
+
+
